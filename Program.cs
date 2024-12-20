704032245098d5fd55b6d1efa6bd0004f70e6fca
Hultberg80 <hultberg80@gmail.com>
2024-12-20 11:02:46 +0100

    eh..

--- Program.cs
+++ Program.cs
@@ -5,17 +5,17 @@ using Npgsql;
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+/*builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();*/
 
 var app = builder.Build();
 
 // Use Swagger during development
-if (app.Environment.IsDevelopment())
+/*if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
     app.UseSwaggerUI();
-}
+}*/
 
 // Serve static files from wwwroot
 app.UseDefaultFiles(); // Serving index.html as the default file
