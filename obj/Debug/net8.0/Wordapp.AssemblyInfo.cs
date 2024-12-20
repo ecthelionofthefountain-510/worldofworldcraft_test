47ffea6ce6eaf0966b4f1285d787a39d6dee5780
Hultberg80 <hultberg80@gmail.com>
2024-12-20 09:31:49 +0100

    first commit

--- obj/Debug/net8.0/Wordapp.AssemblyInfo.cs
+++ obj/Debug/net8.0/Wordapp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Wordapp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b60aeb886691a189d50226d1d34476d07ee7c6fc")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ad32251aecd1981c63bee93bf9a927df6df6d6da")]
 [assembly: System.Reflection.AssemblyProductAttribute("Wordapp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Wordapp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
